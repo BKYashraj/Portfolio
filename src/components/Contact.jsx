08263ff6629cc7001e75ad1cee4d0fbff26804e3
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-03-29 04:25:36 +0530

    done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       name="contact"
       className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
     >
-      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+      <div className="flex flex-col p-4  justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8 pt-10">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
@@ -42,7 +42,7 @@ const Contact = () => {
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
 
-            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 md:py- px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Let's talk
             </button>
           </form>
