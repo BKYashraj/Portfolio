e66f55d3482949de9358f20de28b48867963d3c9
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-11-15 11:56:16 +0530

    added changes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -95,7 +95,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="w-full md:h-screen bg-gradient-to-b from-gray-800 to-black"
+      className="w-full min-h-screen bg-gradient-to-b from-gray-800 to-black"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div className="pt-20">
